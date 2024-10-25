b9a2e41744710dd8387e2c797b23ac32306b51ee
Guillermo <gaponcec@fastmail.mx>
2024-10-25 10:12:37 -0600

    Get Partitions for disks
    Signed-off-by: Guillermo <gaponcec@fastmail.mx>

--- disk-info.py
+++ disk-info.py
@@ -1,15 +1,21 @@
 from diskinfo import Disk, DiskInfo
 
+Disk_Info = {}
+
 di = DiskInfo()
+# get disks
 disks = di.get_disk_list(sorting=True)
 for d in disks:
     dis = Disk(d.get_path().split('/')[-1])
     plist = dis.get_partition_list()
-    print(d.get_path())
+    Disk_Info[d.get_path().split('/')[-1]] = {"path": d.get_path()}
+    Partitions = []
     for p in plist:
-        print(p.get_name())
+        Partitions+=[p.get_name()]
+    Disk_Info[d.get_path().split('/')[-1]]["partitions"] = Partitions
 
+print(Disk_Info)
 
 #documents
-https://diskinfo.readthedocs.io/en/latest/intro.html
-https://diskinfo.readthedocs.io/en/latest/api.html
+#https://diskinfo.readthedocs.io/en/latest/intro.html
+#https://diskinfo.readthedocs.io/en/latest/api.html
